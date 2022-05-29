1c1440a37ea0b6b205adc0c2ae9c5483296324e8
Nidhi <nidhisachan91@gmail.com>
2022-05-29 11:14:55 +1000

    main | added detox e2e test

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -4,10 +4,26 @@ describe('Example', () => {
   });
 
   beforeEach(async () => {
-    await device.reloadReactNative();
+     await device.reloadReactNative();
   });
 
-  it('should show hello screen after tap', async () => {
-    await expect(element(by.text('Step One'))).toBeVisible();
+
+  it('App should launch the home screen', async () => {
+    // Home title should be visible
+    await expect(element(by.id('homeTitle'))).toBeVisible();
+  });
+  it("Test 'Go to Store' button functionality", async () => {
+    // 'Go to Store' button should be visible
+    await expect(element(by.id('storeBtn'))).toBeVisible();
+    // Tap on 'Go to Store' button
+    await element(by.id('storeBtn')).tap();
+    // It should navigate to store screen and validate store title is visible
+    await expect(element(by.id('storeTitle'))).toBeVisible();
+    // 'Go Back' button should be visible
+    await expect(element(by.id('backBtn'))).toBeVisible();
+    // Tap on 'Go Back' button
+    await element(by.id('backBtn')).tap();
+    // It should navigate to home screen and validate home title is visible
+    await expect(element(by.id('homeTitle'))).toBeVisible();
   });
 });
